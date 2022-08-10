10a5288d94561428cda2a11cac88c0424484daac
Branson Morgan <bransox@comcast.net>
2022-08-09 21:30:23 -0400

    commit tree 8deac2f0ec8259f5e4d51085d68716d39422594e

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function distanceTravelledInFeet(a, b) {
         return ((a-b)*264);
     }
 }
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        return ((distance-400)*0.02);
+    } else if (distance >= 2000 && distance <= 2500) {
+        return (25);
+    } else if (distance > 2500) {
+        return ('cannot travel that far');
+    }
+
+
+}

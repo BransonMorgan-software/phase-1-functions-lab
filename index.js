29fd8fe8e042fcf881b814ba4d7a9a12fb746522
Branson Morgan <bransox@comcast.net>
2022-08-09 21:06:45 -0400

    distance in blocks

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street) {
+    if (street<42) {
+        return (42-street);
+    } else {
+        return (street-42);
+    }
+}
